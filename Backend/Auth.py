89704129b223b7f27e9076597bdda7b6d777de16
Pierg01 <piergiuseppe.ligori@gmail.com>
2024-11-07 09:52:59 +0100

    Implementazione parte login

--- Backend/Auth.py
+++ Backend/Auth.py
@@ -2,6 +2,7 @@ import os.path
 
 from flask import Flask, render_template, request, redirect, url_for
 
+from Backend.Utente import Utente
 
 app = Flask(__name__, template_folder='../templates',static_folder='../static')
 
@@ -20,12 +21,25 @@ def mainpage():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    if request.method == "GET":
+        return render_template('login.html')
+    elif request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        utente= Utente("","")
+        if utente.search({"username":username,"password":password}) is not None:
+            if utente.compare_password(password):
+                
+
+
+
+        
 
 
 @app.route('/registrazione')
 def registrazione():
-    return render_template('register.html')
+    if request.method == "GET":
+        return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Backend/Utente.py
+++ Backend/Utente.py
@@ -1,11 +1,31 @@
+import pymongo
+import pprint
+
+
+
 
 
 class Utente:
 
-    def __init__(self,identity: str,name: str,password: str):
-        self.identity = identity
+    def __init__(self,name: str,password: str):
         self.name=name
         self.password=password
+
+    def search(self,utente: dict):
+        client = pymongo.MongoClient("mongodb://localhost:27017")
+        collection = client["Idp_User"]
+        if collection.find_one({"name":utente["name"]}) is None:
+            return {"name": None, "password": None}
+        else:
+            user = collection.find_one({"name":utente["name"]})
+            self.name = user["name"]
+            self.password = user["password"]
+
+
+    def compare_password(self,password) -> bool:
+        return  password == self.password
+
+
 
 
 
