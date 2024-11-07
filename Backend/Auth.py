054e8887782dd172cf2f276da0e60d72911806ee
Pierg01 <piergiuseppe.ligori@gmail.com>
2024-11-07 11:20:23 +0100

    Utente

--- Backend/Auth.py
+++ Backend/Auth.py
@@ -1,4 +1,5 @@
-import os.path
+import oauthlib.oauth2 as oauth2
+from authlib.common.urls import url_encode
 
 from flask import Flask, render_template, request, redirect, url_for
 
@@ -13,12 +14,6 @@ def mainpage():
     if request.method == "GET":
         return render_template('index.html')
 
-
-    
-
-
-
-
 @app.route('/login')
 def login():
     if request.method == "GET":
@@ -29,7 +24,12 @@ def login():
         utente= Utente("","")
         if utente.search({"username":username,"password":password}) is not None:
             if utente.compare_password(password):
-                
+                oauth2.BackendApplicationClient.prepare_request_body(url_encode({"username":username,"password":password}))
+
+
+
+
+
 
 
 
