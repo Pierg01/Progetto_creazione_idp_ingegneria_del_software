f4ad6ec0c408442d727fcbda6f581b5105ebadb5
roccom03 <r.mitola@studenti.poliba.it>
2024-11-10 19:46:47 +0100

    Generazione e verifica totp, modifica registrazione

--- Backend/2FA_autorization.py
+++ Backend/2FA_autorization.py
@@ -1,16 +1,21 @@
 import pyotp
+import qrcode
 
-secret_key = pyotp.random_base32()  # Generate a random secret key
+secret_key = "Pippo"  # Generate a random secret key
+uri = pyotp.totp.TOTP(secret_key).provisioning_uri(name="Rocco",
+                                                   issuer_name="Roc")
 
-#Generazione del codice TOTP
-def generate_TOTP(secret_key):
-    totp = pyotp.TOTP(secret_key)
+qrcode.make(uri).save("qrcode.png")
 
-#Verifica del codice TOTP
-def verify_TOTP(totp):
-    pyotp.verify(totp)
+#metodo di verifica del totp
+def verify_totp(token):
+    return pyotp.TOTP(secret_key).verify(token)
 
-#Totp via mail
-def sendemail(totp):
-    msg=totp
+
+#se il return è vero allora
+#return render_template('index.html')
+
+#se è falso
+#return render_template('login.html')
+
 
