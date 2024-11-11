15b68b89c3dafa30e7c5aba526ce2b308153ba0b
Pierg01 <piergiuseppe.ligori@gmail.com>
2024-11-11 19:10:16 +0100

    Rifinimento Auth

--- Backend/Auth.py
+++ Backend/Auth.py
@@ -1,14 +1,14 @@
 import requests
 import oauthlib.oauth2 as oauth2
 from flask import Flask, render_template, request, redirect, url_for
+from jwt import PyJWT
 from oauthlib.common import generate_client_id
 from oauthlib.oauth2 import BackendApplicationClient
-from oauthlib.openid.connect.core.tokens import JWTToken
+import jwt
+import Utente
+import autorization
 
-from Backend.Utente import Utente
-
-app = Flask(__name__, template_folder='../templates',static_folder='../static')
-
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
 
 @app.route('/')
@@ -16,48 +16,38 @@ def mainpage():
     if request.method == "GET":
         return render_template('index.html')
 
-@app.route('/login',methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "GET":
         return render_template('login.html')
     elif request.method == "POST":
         username = request.form['username']
         password = request.form['password']
-        utente = Utente("","")
-        utente.search({'username':username,'password':password})
-        if utente.compare_password(password):
-            print(utente.compare_password(password))
-            token = JWTToken(request_validator=utente.compare_password(password))
+        utente = Utente.Utente("", "")
+        utente.search({'username': username, 'password': password})
+        if utente.compare_password(utente.password):
+            jwtt = PyJWT()
+            token = jwtt.encode(payload={"Username": utente.Username, "Password": utente.password}, key="secret",
+                                algorithm='HS256')
 
-            token.create_token(utente,refresh_token=True)
-            return requests.get("",params={"token": token})
-    return redirect(url_for('login'))
+            return redirect(url_for("token_generator", strtoken=token))
+        return redirect(url_for('login'))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@app.route('/registrazione')
+@app.route('/registrazione', methods=['GET', 'POST'])
 def registrazione():
     if request.method == "GET":
         return render_template('register.html')
 
-@app.route('/token',methods=['GET'])
+
+@app.route('/token', methods=['GET'])
 def token_generator():
+    token = request.args.get('strtoken')
     if request.method == "GET":
-        return render_template('token_generator.html')
+        return render_template('token_generator.html', strtoken=token)
+
 
 if __name__ == '__main__':
     FLASK_APP = "./Backend/Auth.py"
 
-    app.run(debug=True)
+    app.run(port=3000, debug=True)
